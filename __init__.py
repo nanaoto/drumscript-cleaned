4cd64f23603e9fb3cd95ed8eaae1d9b28aa84ca7
victoria-mckinney <victoria.mckinney@digital.cabinet-office.gov.uk>
2025-06-19 15:51:05 +0100

    amending utils/__init__.py

--- __init__.py
+++ __init__.py
@@ -6,8 +6,3 @@ DrumScript: A Python package to convert drum audio recordings into sheet music.
 
 # Import the install_ffmpeg function to make it directly accessible via drumscript.install_ffmpeg()
 from .utils.ffmpeg_installer import install_ffmpeg
-
-# You might also want to expose other main functions/classes here later
-# For example:
-# from .audio_processor.audio_loader import load_audio
-# from .main import process_drum_track # If main.py becomes the primary entry point function
