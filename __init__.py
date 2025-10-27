4f08f7451a0d3e8e225e4978b2c0dfbbfeb67dd3
victoria-mckinney <60712369+victoria-mckinney@users.noreply.github.com>
2025-10-27 13:47:48 +0000

    restructuring package directories

--- __init__.py
+++
@@ -1,8 +0,0 @@
-# DrumScript/__init__.py
-
-"""
-DrumScript: A Python package to convert drum audio recordings into sheet music.
-"""
-
-# Import the install_ffmpeg function to make it directly accessible via drumscript.install_ffmpeg()
-from .utils.ffmpeg_installer import install_ffmpeg
