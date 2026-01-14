a916f0c9882d2aa9f7e029683886d9668ec97a6c
victoria-mckinney <60712369+victoria-mckinney@users.noreply.github.com>
2026-01-14 16:36:58 +0000

    rebuilding

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 html_theme = 'shibuya'
 html_static_path = ['_static']
-html_css_files = 'custom.css'
+html_css_files = ['custom.css']
 
 # Shibuya Setup
 html_theme_options = {
